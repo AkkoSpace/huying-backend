a1661e938699fbbf4d14ccf57e079e029afddc84
yantingwei <akkoyan@qq.com>
2023-12-26 16:16:17 +0800

    :card_file_box: chore: 更新建表语句

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,4 +1,4 @@
-# 建表脚本
+-- 建表脚本
 
 -- 创建库
 create database if not exists huying;
@@ -7,59 +7,89 @@ create database if not exists huying;
 use huying;
 
 -- 用户表
-create table if not exists user
+create table user
 (
-    id           bigint auto_increment comment 'id' primary key,
-    userAccount  varchar(256)                           not null comment '账号',
-    userPassword varchar(512)                           not null comment '密码',
-    unionId      varchar(256)                           null comment '微信开放平台id',
-    mpOpenId     varchar(256)                           null comment '公众号openId',
-    userName     varchar(256)                           null comment '用户昵称',
-    userAvatar   varchar(1024)                          null comment '用户头像',
-    userProfile  varchar(512)                           null comment '用户简介',
-    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    index idx_unionId (unionId)
-) comment '用户' collate = utf8mb4_unicode_ci;
+    id           bigint auto_increment comment '用户 ID'
+        primary key,
+    userAccount  varchar(32)                                                   not null comment '账号',
+    userPassword varchar(512)                                                  not null comment '密码',
+    uid          varchar(256)                                                  null comment 'UID',
+    userName     varchar(32)                                                   null comment '用户昵称',
+    userAvatar   varchar(1024) default 'https://akko.space/upload/default.png' null comment '用户头像',
+    userProfile  varchar(512)                                                  null comment '用户简介',
+    userRole     varchar(256)  default 'user'                                  not null comment '用户角色：user/admin/ban',
+    createTime   datetime      default CURRENT_TIMESTAMP                       not null comment '创建时间',
+    updateTime   datetime      default CURRENT_TIMESTAMP                       not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint       default 0                                       not null comment '是否删除'
+)
+    comment '用户' collate = utf8mb4_unicode_ci;
 
--- 帖子表
-create table if not exists post
-(
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
-    index idx_userId (userId)
-) comment '帖子' collate = utf8mb4_unicode_ci;
+create index idx_uid
+    on user (uid);
 
--- 帖子点赞表（硬删除）
-create table if not exists post_thumb
+-- 交易表
+create table transaction
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
-    index idx_userId (userId)
-) comment '帖子点赞';
+    id               int auto_increment comment '自增主键'
+        primary key,
+    transactionOrder varchar(255)                       not null comment '交易 ID',
+    transactionDate  date                               not null comment '交易日期',
+    amount           decimal(10, 2)                     not null comment '交易金额',
+    orderId          varchar(255)                       null comment '订单 ID',
+    description      varchar(255)                       null comment '描述',
+    userId           bigint                             not null comment '操作用户 ID',
+    status           tinyint  default 0                 not null comment '交易状态：0-未付款，1-已付款，2-已取消，3-已退款',
+    createTime       datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime       datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete         tinyint  default 0                 not null comment '是否删除'
+)
+    comment '交易' collate = utf8mb4_unicode_ci;
 
--- 帖子收藏表（硬删除）
-create table if not exists post_favour
+-- 产品表
+create table products
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
-    index idx_userId (userId)
-) comment '帖子收藏';
+    id          int auto_increment comment '自增主键'
+        primary key,
+    brand       varchar(255)                       null comment '品牌',
+    productName varchar(255)                       not null comment '产品名称',
+    productDate date                               not null comment '生产日期',
+    barCode     varchar(255)                       null comment '条形码',
+    productSpec varchar(255)                       not null comment '产品规格',
+    productUnit varchar(255)                       not null comment '产品单位',
+    unitPrice   decimal(10, 2)                     not null comment '产品单价',
+    userId      bigint                             not null comment '操作用户 ID',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除'
+)
+    comment '产品' collate = utf8mb4_unicode_ci;
+
+-- 订单表
+create table transaction_products
+(
+    id            int auto_increment comment '自增主键'
+        primary key,
+    transactionId varchar(255)                       not null comment '交易 ID',
+    products      json                               not null comment '订单产品',
+    userId        bigint                             not null comment '操作用户 ID',
+    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete      tinyint  default 0                 not null comment '是否删除'
+)
+    comment '订单产品' collate = utf8mb4_unicode_ci;
+
+-- 公司表
+create table companies
+(
+    id             int auto_increment comment '自增主键'
+        primary key,
+    companyName    varchar(255)                       not null comment '公司名称',
+    companyAddress varchar(255)                       null comment '公司地址',
+    contactPhone   varchar(20)                        null comment '联系电话',
+    contactEmail   varchar(255)                       null comment '联系邮箱',
+    userId         bigint                             not null comment '操作用户 id',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete       tinyint  default 0                 not null comment '是否删除'
+)
+    comment '公司' collate = utf8mb4_unicode_ci;
