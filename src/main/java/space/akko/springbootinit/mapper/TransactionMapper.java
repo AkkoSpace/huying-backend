a2f00d2244d823be406676dcd4fdce6d786f857b
yantingwei <akkoyan@qq.com>
2023-12-26 17:30:54 +0800

    :card_file_box: chore: 更新交易表相关文件

--- src/main/java/space/akko/springbootinit/mapper/TransactionMapper.java
+++ src/main/java/space/akko/springbootinit/mapper/TransactionMapper.java
@@ -4,11 +4,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import space.akko.springbootinit.model.entity.Transaction;
 
 /**
-* @author Akko
-* @description 针对表【transaction(交易)】的数据库操作Mapper
-* @createDate 2023-12-17 14:32:54
-* @Entity space.akko.springbootinit.model.entity.Transaction
-*/
+ * @author Administrator
+ * @description 针对表【transaction(交易)】的数据库操作Mapper
+ * @createDate 2023-12-26 17:21:47
+ * @Entity space.akko.springbootinit.model.entity.Transaction
+ */
 public interface TransactionMapper extends BaseMapper<Transaction> {
 
 }
