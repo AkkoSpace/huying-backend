0aa1771d52188ff1f7d031b0c3adb856e57cf8f3
yantingwei <akkoyan@qq.com>
2024-01-16 09:36:38 +0800

    :sparkles: feat: 使用更加优雅的参数校验
    引入 SpringBoot Validation 并修改全局统一异常处理

--- src/main/java/space/akko/springbootinit/exception/GlobalExceptionHandler.java
+++ src/main/java/space/akko/springbootinit/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package space.akko.springbootinit.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import space.akko.springbootinit.common.BaseResponse;
 import space.akko.springbootinit.common.ErrorCode;
 import space.akko.springbootinit.common.ResultUtils;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Objects;
 
 /**
  * 全局异常处理器
@@ -24,5 +28,17 @@ public class GlobalExceptionHandler {
     public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
         log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, ErrorCode.SYSTEM_ERROR.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public BaseResponse<?> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException", e);
+        return ResultUtils.error(ErrorCode.PARAMS_ERROR, Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public BaseResponse<?> constraintViolationExceptionHandler(ConstraintViolationException e) {
+        log.error("ConstraintViolationException", e);
+        return ResultUtils.error(ErrorCode.PARAMS_ERROR, e.getMessage());
     }
 }
